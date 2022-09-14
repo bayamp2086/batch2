2a6cd5a0c1cd08391db29663d70ae6e3ce8627cc
Harish Appannagari <>
2022-09-13 18:59:22 -0700

    1. added new files. HelloWorld.java 2. Made changes to MyTest.java

--- MavenProject/src/test/java/com/bayamp/trainning/junebatch/stringutils/tests/HelloWorld.java
+++ MavenProject/src/test/java/com/bayamp/trainning/junebatch/stringutils/tests/HelloWorld.java
@@ -1,11 +1,10 @@
 package com.bayamp.trainning.junebatch.stringutils.tests;
 
-public class MyTest {
+public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		System.out.println("I am changing this messsage");
+		System.out.println("hello");
 	}
 
 }
